68d912c8cdcfa810f4525d35d32eaf1ba0b131f4
NeilsUltimateLab <neilsultimatelab@icloud.com>
2018-06-10 18:03:33 +0530

    set background colour

--- SourceControlDemo/ViewController.swift
+++ SourceControlDemo/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func setupViews() {
+        self.view.backgroundColor = UIColor.groupTableViewBackground
+    }
 
 }
 
